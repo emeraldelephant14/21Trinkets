688a31554b030271565a2a52640e00c7f0719456
ornateowl <139507059+ornateowl@users.noreply.github.com>
2026-04-07 22:47:08 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -72,7 +72,7 @@ const themes = {
       { src: "images/friend_2.png", price: "$16" },
       { src: "images/friend_3.png", price: "$16" },
       { src: "images/friend_4.png", price: "$16" },
-      { src: "images/friend_5.png", prince = "16" }
+      { src: "images/friend_5.png", prince: "16" }
     ]
   }
   
