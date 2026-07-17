275c5be22f7d18d9661ae62c1b9332f6080257a7
emeraldelephant14 <139507004+emeraldelephant14@users.noreply.github.com>
2026-07-17 15:56:44 -0700

    Update prices for various earrings in gallery

--- gallery2.js
+++ gallery2.js
@@ -11,16 +11,16 @@ const themes = {
     title: "Fruit + Dessert Earrings",
     items: [
       {src: "images/dessert_1.png", price: "$4"},
-      {src: "images/dessert_2.png", price: "$5"},
+      {src: "images/dessert_2.png", price: "$6"},
       {src: "images/dessert_3.png", price: "$5"},
       {src: "images/dessert_4.png", price: "$6"},
-      {src: "images/dessert_5.png", price: "$6"},
+      {src: "images/dessert_5.png", price: "$7"},
       {src: "images/creamsicle.png", price: "$4"},
-      {src: "images/dessert6.png", price: "$2"},
-      {src: "images/dessert_7.png", price: "$2"},
+      {src: "images/dessert6.png", price: "$3"},
+      {src: "images/dessert_7.png", price: "$3"},
       {src: "images/dessert_8.png", price: "$4"},
       {src: "images/dessert_9.png", price: "$3"},
-      {src: "images/dessert_10.png", price: "$5"},
+      {src: "images/dessert_10.png", price: "$6"},
       {src: "images/dessert_11.png", price: "$6"}
       
 
@@ -30,10 +30,10 @@ const themes = {
     title: "Celestial Earrings",
     items: [
       {src: "images/celestial_1.png", price: "$6"},
-      {src: "images/star.png", price: "$2"},
-      {src: "images/planet.png", price: "$2"},
-      {src: "images/celestial2.png", price: "$2"},
-      {src: "images/celestial3.png", price: "$2"},
+      {src: "images/star.png", price: "$3"},
+      {src: "images/planet.png", price: "$3"},
+      {src: "images/celestial2.png", price: "$3"},
+      {src: "images/celestial3.png", price: "$3"},
       {src: "images/celestial4.png", price: "$8"},
       {src: "images/celestial_5.png", price: "$3"},
       {src: "images/celestial_6.png", price: "$3"}
@@ -44,13 +44,13 @@ const themes = {
   spring: {
     title: "Spring Earrings",
     items: [
-      {src: "images/spring_1.png", price: "$2"},
-      {src: "images/spring_2.png", price: "$2"},
-      {src: "images/spring_3.png", price: "$2"},
-      {src: "images/spring_4.png", price: "$2"},
+      {src: "images/spring_1.png", price: "$3"},
+      {src: "images/spring_2.png", price: "$3"},
+      {src: "images/spring_3.png", price: "$3"},
+      {src: "images/spring_4.png", price: "$3"},
       {src: "images/spring_5.png", price: "$4"},
-      {src: "images/spring_6.png", price: "$2"},
-      {src: "images/spring_7.png", price: "$2"},
+      {src: "images/spring_6.png", price: "$3"},
+      {src: "images/spring_7.png", price: "$3"},
       {src: "images/spring_8.png", price: "$5"},
       {src: "images/spring_9.png", price: "$5"}
     ]
@@ -59,23 +59,23 @@ const themes = {
   sea: {
     title: "Sea Earrings",
     items: [
-      {src: "images/sea_1.png", price: "$2"},
-      {src: "images/sea_2.png", price: "$2"},
-      {src: "images/sea_3.png", price: "$2"},
-      {src: "images/sea_4.png", price: "$3"},
+      {src: "images/sea_1.png", price: "$3"},
+      {src: "images/sea_2.png", price: "$3"},
+      {src: "images/sea_3.png", price: "$3"},
+      {src: "images/sea_4.png", price: "$4"},
       {src: "images/sea_5.png", price: "$3"},
-      {src: "images/sea_6.png", price: "$2"}
+      {src: "images/sea_6.png", price: "$3"}
     ]
   },
 
   enchanted: {
     title: "Enchanted Forest Earrings",
     items: [
-      {src: "images/enchanted_1.png", price: "$2"},
+      {src: "images/enchanted_1.png", price: "$3"},
       {src: "images/enchanted_2.png", price: "$4"},
       {src: "images/enchanted_3.png", price: "$5"},
       {src: "images/enchanted_4.png", price: "$3"},
-      {src: "images/enchanted_5.png", price: "$2"},
+      {src: "images/enchanted_5.png", price: "$3"},
       {src: "images/enchanted_6.png", price: "$3"},
       {src: "images/enchanted_7.png", price: "$3"},
       {src: "images/enchanted_8.png", price: "$3"}
