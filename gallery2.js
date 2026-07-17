432f028b5860c213dbb2654cf9ef57fb85dc9735
ornateowl <139507059+ornateowl@users.noreply.github.com>
2026-07-17 16:49:04 -0700

    Add photos of doughnuts and haunter

--- gallery2.js
+++ gallery2.js
@@ -21,7 +21,9 @@ const themes = {
       {src: "images/dessert_8.png", price: "$4"},
       {src: "images/dessert_9.png", price: "$3"},
       {src: "images/dessert_10.png", price: "$6"},
-      {src: "images/dessert_11.png", price: "$6"}
+      {src: "images/dessert_11.png", price: "$6"},
+      {src: "images/dessert_12.png", price: "$6"},
+      {src: "images/dessert_13.png", price: "$6"}
       
 
     ]
